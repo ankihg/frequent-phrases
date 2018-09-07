bd2ca45fed2f6e6bab611419b96d178a452bb30f
Anki <anki@Ankis-MacBook-Pro.local>
2018-09-07 14:46:20 -0700

    desired output achieved

--- run3.js
+++ run3.js
@@ -1,6 +1,6 @@
 const natural = require('natural');
 const MIN_GRAM = 3;
-const MAX_GRAM = 5;
+const MAX_GRAM = 10;
 
 /**
 NOTE NOT GOOD ALGORITHM OR STRUCTURE
@@ -42,8 +42,9 @@ let result = sentences.reduce((lookup, sentence) => {
     console.log(grams);
 
     grams.forEach((gram) => {
-        lookup[gram] = lookup[gram] || 0;
-        lookup[gram]++;
+        let key = _key(gram);
+        lookup[key] = lookup[key] || 0;
+        lookup[key]++;
     });
 
 
@@ -55,12 +56,36 @@ let result = sentences.reduce((lookup, sentence) => {
 // console.log(JSON.stringify(result, null, 4));
 
 let sorted = Object.keys(result)
-                .sort((aGram, bGram) =>  result[aGram] - result[bGram])
-                .map((gram) => { return { gram: gram, count: result[gram] } });
+                .sort((aGram, bGram) =>  result[bGram] - result[aGram])
+                .map((gram) => { return { gram: gram, count: result[gram], length: gram.split('|').length } });
 
 console.log('\n\n\n');
-
 console.log(JSON.stringify(sorted, null, 4));
+
+let top_frequent = sorted.slice(0, 10);
+
+console.log('\n\n\n');
+console.log(JSON.stringify(top_frequent, null, 4));
+
+let topSortedByLength = top_frequent.sort((a, b) => b.length - a.length);
+
+console.log('\n\n\n');
+console.log(JSON.stringify(topSortedByLength, null, 4));
+
+// remove subphrases
+let fitleredFrequentPhrases = topSortedByLength
+  .filter((potentialSubGram, i) => {
+        for (let j = 0; j < i; j++) {
+          let potentialSuperGram = topSortedByLength[j];
+          if (potentialSubGram.length < potentialSuperGram.length && potentialSuperGram.gram.includes(potentialSubGram.gram))
+            return false;
+        }
+        return true;
+  });
+
+console.log('\n\n\n');
+console.log(JSON.stringify(fitleredFrequentPhrases, null, 4));
+
 
 // let grams = natural.NGrams.ngrams('some other words here for you', 4)
 // console.log(grams);
@@ -73,4 +98,8 @@ function _generateGrams(sentence) {
         grams = grams.concat(natural.NGrams.ngrams(sentence, n));
     }
     return grams;
+}
+
+function _key(gram) {
+  return gram.join('|');
 }
