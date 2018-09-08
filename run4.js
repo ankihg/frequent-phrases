5cec20bcb8f15ff6d9646e85b3e7ac19e49998b3
Anki <anki@Ankis-MacBook-Pro.local>
2018-09-08 16:18:19 -0700

    improve sentencd parsing

--- run4.js
+++ run4.js
@@ -1,11 +1,12 @@
+'use strict';
+module.exports = exec;
+
 const natural = require('natural');
-const MIN_GRAM = 3;
-const MAX_GRAM = 10;
 
 /**
 TODO idea, plz shoot holes
 
-gen all grams from MIN_GRAM to MAX_GRAM
+gen all grams from options.MIN_GRAM to options.MAX_GRAM
 build hash of grams to count=0 [countsByGram]
 build gramOrder array that has grams by length, longests first
 for (gram in gramOrder)
@@ -18,61 +19,64 @@ for (gram in gramOrder)
 
 **/
 
+function exec(docStr, options={MIN_GRAM: 3, MAX_GRAM: 10}) {
+    console.log(docStr);
+    console.log(options);
+    let grams = _generateGrams(docStr);
 
-let docStr = 'The quick brown fox jumped over the lazy dog. The lazy dog, peeved to be labeled lazy, jumped over a snoring turtle. In retaliation the quick brown fox jumped over ten snoring turtles.';
-docStr = docStr.toLowerCase(); // TODO discuss handling to capitalization
-let grams = _generateGrams(docStr);
+    grams.inDescLength.forEach((gramKey) => {
+        if (! (gramKey in grams.countsByGram)) return;
 
-grams.inDescLength.forEach((gramKey) => {
-    if (! (gramKey in grams.countsByGram)) return;
+        grams.countsByGram[gramKey]++;
+        if (grams.countsByGram[gramKey] > 1)
+            _deleteSubgrams(gramKey, grams.countsByGram);
+    });
 
-    grams.countsByGram[gramKey]++;
-    if (grams.countsByGram[gramKey] > 1)
-        _deleteSubgrams(gramKey, grams.countsByGram);
-});
+    let repeatedPhrases = _filterSingles(grams.countsByGram).map((gramKey) => _splitKey(gramKey).join(' '));
+    return repeatedPhrases;
 
-let repeatedPhrases = _filterSingles(grams.countsByGram).map((gramKey) => _splitKey(gramKey).join(' '));
-
-console.log(JSON.stringify(repeatedPhrases, null, 4));
+    console.log(JSON.stringify(repeatedPhrases, null, 4));
 
 
-function _generateGrams(docStr) {
-    let grams = { countsByGram: {}, inDescLength: [] };
-    let sentences = docStr.split('. '); // TODO better sentence parser
+    function _generateGrams(docStr) {
+        let grams = { countsByGram: {}, inDescLength: [] };
+        let sentences = docStr.split(/\.\s+/); // TODO better sentence parser
+        console.log('sentences',sentences); // TODO TODO TODO NOW address sentence split failure
+        for (let n = options.MAX_GRAM; n >= options.MIN_GRAM; n--) {
+            sentences.forEach((sentence) => {
+                natural.NGrams.ngrams(sentence, n)
+                    .forEach((gram) => {
+                        let gramKey = _key(gram);
+                        grams.countsByGram[gramKey] = 0;
+                        grams.inDescLength.push(gramKey);
+                    });
+            });
+        }
+        // console.log(JSON.stringify(grams.inDescLength, null, 4));
+        return grams;
+    }
 
-    for (let n = MIN_GRAM; n <= MAX_GRAM; n++) {
-        sentences.forEach((sentence) => {
-            natural.NGrams.ngrams(sentence, n)
+    function _deleteSubgrams(gramKey, keyHolder) {
+        let gram = _splitKey(gramKey);
+        for (let n = options.MIN_GRAM; n < gram.length; n++) {
+            natural.NGrams.ngrams(gram, n)
                 .forEach((gram) => {
                     let gramKey = _key(gram);
-                    grams.countsByGram[gramKey] = 0;
-                    grams.inDescLength.push(gramKey);
+                    delete keyHolder[gramKey];
                 });
-        });
+        }
     }
-    return grams;
-}
 
-function _deleteSubgrams(gramKey, keyHolder) {
-    let gram = _splitKey(gramKey);
-    for (let n = MIN_GRAM; n < gram.length; n++) {
-        natural.NGrams.ngrams(gram, n)
-            .forEach((gram) => {
-                let gramKey = _key(gram);
-                delete keyHolder[gramKey];
-            });
+    function _filterSingles(countsByGram) {
+        return Object.keys(countsByGram)
+                    .filter((gramKey) => countsByGram[gramKey] > 1);
     }
-}
 
-function _filterSingles(countsByGram) {
-    return Object.keys(countsByGram)
-                .filter((gramKey) => countsByGram[gramKey] > 1);
-}
+    function _key(gram) {
+      return gram.join('|');
+    }
 
-function _key(gram) {
-  return gram.join('|');
-}
-
-function _splitKey(gramKey) {
-  return gramKey.split('|');
+    function _splitKey(gramKey) {
+      return gramKey.split('|');
+    }
 }
