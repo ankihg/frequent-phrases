1e980e2af5d54c785cdc78176f43b5864c02cb23
Anki <anki@Ankis-MacBook-Pro.local>
2018-09-08 16:55:42 -0700

    cleanup run3 code

--- run3.js
+++ run3.js
@@ -10,7 +10,6 @@ function exec(docStr, options={MIN_GRAM: 3, MAX_GRAM: 10}) {
 
     let result = sentences.reduce((lookup, sentence) => {
         let grams = _generateGrams(sentence);
-        // console.log(grams);
 
         grams.forEach((gram) => {
             let key = _key(gram);
@@ -18,53 +17,22 @@ function exec(docStr, options={MIN_GRAM: 3, MAX_GRAM: 10}) {
             lookup[key]++;
         });
 
-
-        // console.log(grams);
-        // console.log('\n\n\n');
         return lookup;
     }, {});
 
-    let output = Object.keys(result)
-                            .filter((gramKey) => result[gramKey] > 1)
-                            .sort((aGramKey, bGramKey) =>  _splitKey(bGramKey).length - _splitKey(aGramKey).length)
-                            .filter((potentialSubGram, i, phrasesInDescLength) => {
-                                console.log('phrasesInDescLength', i);
-                                console.log(phrasesInDescLength);
-                                for (let j = 0; j < i; j++) {
-                                    let potentialSuperGram = phrasesInDescLength[j];
-                                    if (potentialSubGram.length < potentialSuperGram.length && potentialSuperGram.includes(potentialSubGram))
-                                        return false;
-                                }
-                                return true;
-                            })
-                            .map((gramKey) => _splitKey(gramKey).join(' '));
-
-    console.log('output');
-    console.log(output);
-    return output;
-
-
-    let sorted = Object.keys(result)
-                    .sort((aGram, bGram) =>  result[bGram] - result[aGram])
-                    .map((gram) => { return { gram: gram, count: result[gram], length: gram.split('|').length } })
-                    .map;
-
-    let top_frequent = sorted.slice(0, 10);
-
-    let topSortedByLength = top_frequent.sort((a, b) => b.length - a.length);
-
-    // remove subphrases
-    let fitleredFrequentPhrases = topSortedByLength
-      .filter((potentialSubGram, i) => {
-            for (let j = 0; j < i; j++) {
-              let potentialSuperGram = topSortedByLength[j];
-              if (potentialSubGram.length < potentialSuperGram.length && potentialSuperGram.gram.includes(potentialSubGram.gram))
-                return false;
-            }
-            return true;
-      });
-
-     return fitleredFrequentPhrases;
+    return Object.keys(result)
+                    .filter((gramKey) => result[gramKey] > 1)
+                    .sort((aGramKey, bGramKey) =>  _splitKey(bGramKey).length - _splitKey(aGramKey).length)
+                    .filter((potentialSubGram, i, phrasesInDescLength) => {
+                        for (let j = 0; j < i; j++) { // TODO switch to while loop
+                            let potentialSuperGram = phrasesInDescLength[j];
+                            if (potentialSubGram.length >= potentialSuperGram.length) break;
+                            if (potentialSuperGram.includes(potentialSubGram))
+                                return false;
+                        }
+                        return true;
+                    })
+                    .map((gramKey) => _splitKey(gramKey).join(' '));
 
 
     function _generateGrams(sentence) {
