8a631b1fea0c45c2177ccfe03e4d71c5ff9df28a
Anki <anki@Ankis-MacBook-Pro.local>
2018-09-08 17:26:48 -0700

    assert expected output fn

--- test/test.js
+++ test/test.js
@@ -11,11 +11,13 @@ const solutions = {
 };
 
 const resources = {
-    'brown-fox': '["the quick brown fox jumped over","the lazy dog"]',
-    'lazy-dog': '["the quick brown fox jumped over","over the lazy dog","the quick fox"]',
-    'sentence-division': '["the quick brown fox jumped over","peeved to be"]',
+    'brown-fox': ["the quick brown fox jumped over","the lazy dog"],
+    'lazy-dog': ["the quick brown fox jumped over","over the lazy dog","the quick fox"],
+    'sentence-division': ["the quick brown fox jumped over","peeved to be", "plz"],
+    'nytimes-oped': '[]',
 };
 
+const performance = {};
 
 describe('test algorithms', () => {
 
@@ -25,15 +27,30 @@ describe('test algorithms', () => {
             let documentString = data.toString();
             documentString = documentString.toLowerCase(); // TODO discuss handling to capitalization
 
+            performance[filename] = {};
             _.each(solutions, (execSolution, solutionKey) => {
 
+                let startTime = Date.now();
                 let output = execSolution(documentString);
-                expect(JSON.stringify(output)).equal(expectedOutput);
-                console.log(filename, solutionKey, JSON.stringify(output));
-            });
+                let endTime = Date.now();
 
+                console.log(_isUnexpectedOutput(expectedOutput, output));
+                expect(_isUnexpectedOutput(expectedOutput, output)).not.exist;
+                performance[filename][solutionKey] = endTime - startTime;
+
+            });
         });
+
+        console.log('\n\n\n');
+        console.log(JSON.stringify(performance, null, 4));
     });
 
 
 });
+
+function _isUnexpectedOutput(expected, output) {
+    if (output.length !== expected.length) return { expected, output };
+    let outputValueHash = output.reduce((hash, value) => { hash[value] = true; return hash }, {});
+    for (let expectedValue of expected)
+        if (! (expectedValue in outputValueHash)) return { expected, output };
+}
