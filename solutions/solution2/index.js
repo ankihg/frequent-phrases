adc16d9ccad52e6e0876ad0687e92a840094d889
Anki <anki@Ankis-MacBook-Pro.local>
2018-09-09 15:55:14 -0700

    updating solution descriptions

--- solutions/solution2/index.js
+++ solutions/solution2/index.js
@@ -2,14 +2,14 @@
 module.exports = {
     exec: require('./exec'),
     description: `
-        The goal with this approach is to avoid collecting subphrases of repeated superphrases so they don't have to be filtered out at the end.
+        The goal with this approach is to avoid collecting subphrases of repeated phrases so they don't have to be filtered out at the end.
 
         This requires visiting longer phrases first, so you know what subphrases to avoid collecting.
 
-        The \`_generateGrams\` helper function handles this by returning an array (\`inDescLength\`) with longer phrases first.
+        The \`_generateGrams\` helper function handles this by returning an array (\`inDescLength\`) with longer phrases first. This will be the order that we iterate through the phrases in.
 
 
-
+        A possible improvement to the efficiency would be, at phrase generation time, to build a lookup from a phrase to its subphrases. That way, when you see that a phrase has repeated you can lookup which subphrases to delete from the valid phrases lookup, instead of regenerating the subphrases for that phrase, as the algorithm is currently doing. This would require writing a custom ngram generation function, sacrificing maintainability, so I've decided not to implement.
     `,
     pseudo: `
         gen all grams from MIN_GRAM to MAX_GRAM
---
+++ solutions/solution3/index.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = {
+    // exec: require('./exec'),
+    description: `
+        My goal is to collect the phrase counts in a data structure that natively handles storing subphrases beneath their superphrases to avoid the awkward subphrase deletion step of solution2.
+
+        This would require traversing the data structure at the end to get subphrases for non-repeated phrases.
+
+    `,
+    pseudo: `
+
+    `,
+};
